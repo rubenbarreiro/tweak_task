bb58ccffd4ee139fda70b1d2888727b86c88a398
Ruben Barreiro <ruben.barreirom@gmail.com>
2020-08-22 17:09:09 -0500

    working exif locally, will try to solve the problem in lambda

---
+++ bin/README.md
@@ -0,0 +1,5 @@
+### What is this?
+
+This folder is meant to have the binaries required for the task to run, for each needed you have to download and extract them in the following way also name the folders as this:
+
+* exiftool: https://exiftool.org/Image-ExifTool-12.04.tar.gz
--- handler.js
+++ handler.js
@@ -1,7 +1,12 @@
 'use strict';
+process.env['PATH'] = process.env['PATH'] + ':' + (process.env['LAMBDA_TASK_ROOT'] + '/bin/exiftool')
+
 const aws = require('aws-sdk')
 const multipartParser = require('lambda-multipart-parser');
-const to = require('await-to-js').default
+const to = require('await-to-js').default;
+const fs = require('fs').promises
+const exif = require('exiftool');
+const { exec } = require('child_process');
 
 const uploadFile = (file, filePath) => {
   return new Promise((resolve, reject) => {
@@ -22,44 +27,62 @@ const uploadFile = (file, filePath) => {
   })
 }
 
-module.exports.hello = async (event, context, callback) => {
+const storeExifData = (filePath, data) => {
+  return new Promise(async (resolve, reject) => {
+    /*const fileLocation = `/tmp/${filePath}`
+    console.log('\x1b[33m 33 handler.js > fileLocation === \x1b[0m ', fileLocation)
+    await fs.writeFile(fileLocation, data)*/
+
+    exif.metadata(data, async (err, metaData) => {
+      if (err) {
+        return reject(err)
+      }
+
+      const finalMetaData = {}
+      for (const row in metaData) {
+        finalMetaData[row] = metaData[row]
+      }
+
+      const [errS3] = await to(uploadFile({
+        filename: 'meta_data.json',
+        contentType: 'application/json',
+        content: JSON.stringify(finalMetaData, null, 2)
+      }, filePath))
+      if (errS3) {
+        return reject(errS3)
+      }
+
+      return resolve()
+    })
+  });
+}
+
+const formatResponse = (code, data) => {
+  return {
+    statusCode: code,
+    body: JSON.stringify(data)
+  }
+}
+
+module.exports.fileUpload = async (event, context, callback) => {
   const body = await multipartParser.parse(event);
   const filePath = Date.now();
   const file = body.files[0]
 
   const [errUpload] = await to(uploadFile(file, filePath))
   if (errUpload) {
-    return callback(null, {
-      statusCode: 500,
-      body: JSON.stringify(errUpload)
-    })
+    console.error('file upload error', errUpload);
+    return callback(null, formatResponse(500, errUpload));
   }
 
+  const [errExif] = await to(storeExifData(filePath, file.content));
+  if (errExif) {
+    console.error('exif error', errExif);
+    return callback(null, formatResponse(500, errExif));
+  }
 
   return callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ foo: 'bar' })
+    body: JSON.stringify({ message: 'File uploaded succesfully' })
   })
-
-  /*const body = multipart.parse(event, true)
-  const s3 = new aws.S3()
-  const filePath = Date.now()
-  console.log('\x1b[33m 14 handler.js >  === \x1b[0m ', body)
-
-  fs.writeFileSync(__dirname + '/foo.png', body.file.content)
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  })*/
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
